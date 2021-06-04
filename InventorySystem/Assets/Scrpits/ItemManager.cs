868c5669c426902dfce53490e99428046fe82584
FacundoPonce <mrfacu125@gmail.com>
2021-06-04 18:11:45 -0300

    Created ui and start coding the itemManager.

---
+++ InventorySystem/Assets/Scrpits/ItemManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public List<ItemController> _itemsList;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
