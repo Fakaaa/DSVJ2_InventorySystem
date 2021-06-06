e2ae7a97b36ee399d164925cc94be8229b8a87f0
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-06-05 21:52:58 -0300

    EquipPlace & EquipManager interaction changed. Unequip item added

--- InventorySystem/Assets/Scripts/EquipmentScripts/EquipmentManager.cs
+++ InventorySystem/Assets/Scripts/EquipmentScripts/EquipmentManager.cs
@@ -1,57 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class EquipmentManager : MonoBehaviour
 {
     [SerializeField] InventoryManager inventory;
-    [SerializeField] Button helmetButton;
-    Item helmetItem;
-    [SerializeField] Button chestplateButton;
-    Item chestplateItem;
-    [SerializeField] Button bootsButton;
-    Item bootsItem;
-    [SerializeField] Button weaponButton;
-    Item weaponItem;
+    [SerializeField] EquipmentPlace helmet;
+    [SerializeField] EquipmentPlace chestplate;
+    [SerializeField] EquipmentPlace boots;
+    [SerializeField] EquipmentPlace weapon;
+
+    void Start()
+    {
+        helmet.itemPlace += GetItemToPlace;
+        chestplate.itemPlace += GetItemToPlace;
+        boots.itemPlace += GetItemToPlace;
+        weapon.itemPlace += GetItemToPlace;
+    }
 
     public void ReciveItem(ref Item _itemIn){
         switch (_itemIn.itemType) {
             case Item.ItemType.Weapon:
 
-                ChangeData(ref _itemIn, ref weaponItem, ref weaponButton);
+                weapon.ChangeData(ref _itemIn);
 
             break;
             case Item.ItemType.Helmet:
 
-                ChangeData(ref _itemIn, ref helmetItem, ref helmetButton);
+                helmet.ChangeData(ref _itemIn);
 
             break;
             case Item.ItemType.Chestplate:
 
-                ChangeData(ref _itemIn, ref chestplateItem, ref chestplateButton);
+                chestplate.ChangeData(ref _itemIn);
 
             break;
             case Item.ItemType.Boots:
 
-                ChangeData(ref _itemIn, ref bootsItem, ref bootsButton);
+                boots.ChangeData(ref _itemIn);
 
             break;
         }
     }
 
-    void ChangeData(ref Item _itemIn, ref Item _localItem, ref Button _button){
-        if(_localItem == null){
-            _localItem = _itemIn;
-            _button.image.sprite = _itemIn.icon;
-        }
-        else if(_localItem == _itemIn){
-            inventory.SetItem(_itemIn);
-        }
-        else{
-            inventory.SetItem(_localItem);
-            _localItem = _itemIn;
-            _button.image.sprite = _itemIn.icon;
-        }
+    void GetItemToPlace(ref Item _itemIn){
+        inventory.SetItem(_itemIn);
     }
 }
---
+++ InventorySystem/Assets/Scripts/EquipmentScripts/EquipmentPlace.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EquipmentPlace : MonoBehaviour, IPointerClickHandler
+{
+    public delegate void ItemPlace(ref Item _itemIn);
+    public ItemPlace itemPlace;
+    Item itemAtPlace;
+    Button itemPlaceButton;
+    [SerializeField] Sprite defaultIcon;
+
+    void Start()
+    {
+        itemPlaceButton = GetComponent<Button>();
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData){
+        if (pointerEventData.button == PointerEventData.InputButton.Left) {
+            itemPlace?.Invoke(ref itemAtPlace);
+            itemPlaceButton.image.sprite = defaultIcon;
+            itemAtPlace = null;
+        }
+    }
+
+    public void ChangeData(ref Item _itemIn){
+        if(itemAtPlace == null){
+            itemAtPlace = _itemIn;
+            itemPlaceButton.image.sprite = _itemIn.icon;
+        }
+        else if(itemAtPlace == _itemIn){
+            itemPlace?.Invoke(ref _itemIn);
+        }
+        else{
+            itemPlace?.Invoke(ref itemAtPlace);
+            itemAtPlace = _itemIn;
+            itemPlaceButton.image.sprite = _itemIn.icon;
+        }
+    }
+}
